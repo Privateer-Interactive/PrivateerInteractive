130a33d60d7fc0a073b858f6b44c2ff001f63f8d
eprivateer <ebd.BusinessG@gmail.com>
2025-09-09 11:28:04 -0400

    ***DEAD END*** keep until further notice...

--- AddContent.js
+++ AddContent.js
@@ -15,24 +15,62 @@ function GenerateContainer(mainBody)
 
 
 /* #region AddContent_OnClick */
-function GenerateSection()
+function CheckIfClass(DOMElement, stringName)
 {
-
+    if(DOMElement === undefined || stringName === undefined) throw new console.error("DOMElement or stringName is undefined");
+    if(DOMElement instanceof Element)
+    {
+        if(DOMElement.className !== stringName)
+        {
+            return false;
+        }
+        else return true;
+    }
+    else return false;
+}
+// NEEDS WORK - NOT FUNCTIONAL
+function DOMCheck(DOMElement)
+{
+    if(DOMElement === undefined) throw new console.error("DOMElement is undefined");
+    if(DOMElement instanceof Element) return true;
+    else return false;
 }
 
-function GenerateContainer()
+function ClassExists(ClassName)
+{
+    if(typeof ClassName === 'string')
+    {
+        return document.getElementsByClassName(ClassName).length > 0;
+    }
+    else throw new console.error("ClassName is not a valid string");
+}
+
+function GenerateSection(MainBodyElement)
 {
 
+    
+
+    const sectionDiv = document.createElement('div');
+    sectionDiv.className = 'Section';
+    MainBodyElement.appendChild(sectionDiv);
+}
+function GenerateDOM(ParentDOM, newDOMTypeName, newDOMClassName = null)
+{
+    if(ParentDOM === undefined || newDOMTypeName === undefined) throw new console.error("ParentDOM or newDOMTypeName is undefined");
+    if(!(ParentDOM instanceof Element) || typeof ParentDOM !== 'string') throw new console.error("ParentDOM is not a valid DOM element or selector string");
+}
+function GenerateContainer(SectionElement)
+{
+    if(!CheckIfClass(SectionElement, "Section")) throw new console.error("SectionElement is not a valid Section class element");
+
+    const containerDiv = document.createElement('div');
+    containerDiv.className = 'Container';
+    SectionElement.appendChild(containerDiv);
 }
 
 function OrientContainer(DOMElement)
 {
-    if(DOMElement instanceof Element)
-    {
-        if(DOMElement.className !== "Container")
-        {
-            throw new console.warn("DOMElement is not a valid Container element");
-        }
-    }
+    if(CheckIfClass(DOMElement, "Container")) throw new console.error("DOMElement is not a valid Container class element");
+    
 }
 /* #endregion */
--- README.md
+++ README.md
@@ -2,5 +2,9 @@
 Web page and introduction to the LLC.
 
 Project Page Layout
+CLASSES
 - Container : Holds any number of sections
+    - CSS BASELINE
+        -> display: flex
+        -> border: BorderWidth solid black
 - Section : Holds data, text, images, etc... for display to viewer
