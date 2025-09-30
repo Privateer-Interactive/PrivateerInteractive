c634e48b08a7af0cc342b272798b7d49a688aa0f
eprivateer <ebd.BusinessG@gmail.com>
2025-09-30 14:03:49 -0400

    Test

--- Scripts/AddContent.js
+++ Scripts/AddContent.js
@@ -152,3 +152,5 @@ let EditFunctionArray = [
 
 console.log(EditFunctionArray.length + ' Edit Functions loaded.');
 console.log(typeof EditFunctionArray[0] === 'function' ? 'EditFunctionArray[0] is a function.' : 'EditFunctionArray[0] is not a function.');
+console.log(typeof EditFunctionArray[0]);
+
